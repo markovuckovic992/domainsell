26de8bd72c2a144e29cd4d4bcadb5038314fe9c2
markovuckovic992 <markovuckovic992@yahoo.com>
2016-12-08 22:45:55 +0100

    csrf

--- mails/views.py
+++ mails/views.py
@@ -16,7 +16,7 @@ def receive_mails(request):
     elif entry.offer_id:
         return render_to_response('offer_made.html', {'offer_id': entry.offer_id, 'again': 1})
     else:
-        return render_to_response('offer.html', {'lead': entry.lead, 'base_id': entry.base_id, 'hash': hash_base_id})
+        return render_to_response('offer.html', {'drop': entry.drop, 'base_id': entry.base_id, 'hash': hash_base_id})
 
 def process_offer(request):
     base_id = request.POST['base_id']
