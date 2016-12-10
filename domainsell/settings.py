4d65dc18bafb16a4db6577ef2bea1fe719318daa
markovuckovic992 <markovuckovic992@yahoo.com>
2016-12-10 15:37:26 +0100

    sun final 6

--- domainsell/settings.py
+++ domainsell/settings.py
@@ -140,3 +140,20 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static').replace('\\', '/'),
 )
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+        },
+    },
+}
