839599248e06fc2d95690eb968e3ac28efd34e01
markovuckovic992 <markovuckovic992@yahoo.com>
2016-12-22 11:08:08 +0100

    Signed-off-by: markovuckovic992 <markovuckovic992@yahoo.com>

--- domainsell/settings.py
+++ domainsell/settings.py
@@ -142,11 +142,11 @@ STATICFILES_DIRS = (
 )
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'televizorzid987@gmail.com'
-EMAIL_HOST_PASSWORD = 'parket123'
 EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.zoho.com'
+EMAIL_HOST_USER = 'info@ipenbiz.com'
+EMAIL_HOST_PASSWORD = 'asd12fgh'
+EMAIL_USE_TLS = True
 
 LOGGING = {
     'version': 1,
