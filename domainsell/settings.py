b4b168dfe1ea0fa6705faf88a3cd5214b0783f35
markovuckovic992 <markovuckovic992@yahoo.com>
2017-01-09 12:43:53 +0100

    Signed-off-by: markovuckovic992 <markovuckovic992@yahoo.com>

--- domainsell/settings.py
+++ domainsell/settings.py
@@ -77,10 +77,6 @@ TEMPLATES = [
     },
 ]
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
 WSGI_APPLICATION = 'domainsell.wsgi.application'
 
 
@@ -137,9 +133,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static').replace('\\', '/'),
 )
