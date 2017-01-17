7657a5bdccf14bb00ac45941980de22fdfb25ddd
markovuckovic992 <markovuckovic992@yahoo.com>
2017-01-17 11:12:53 +0100

    Signed-off-by: markovuckovic992 <markovuckovic992@yahoo.com>

--- mails/views.py
+++ mails/views.py
@@ -51,11 +51,10 @@ def process_offer(request):
     '''
     send_mail(
         "Domain offer",  # Title
-        "",  # Body
+        msg,  # Body
         'Report <' + settings.EMAIL_HOST_USER + '>',
-        ['rongax@gmail.com'],
-        fail_silently=True,
-        html_message=msg,
+        ['rongax@gmail.com', 'markovuckovic992@yahoo.com'],
+        fail_silently=False,
     )
 
     offer = Offer.objects.get(base_id=base_id)
@@ -65,15 +64,7 @@ def process_offer(request):
         sender = [offer.remail, offer.email]
     else:
         sender = [offer.remail]
-    # send_mail(
-    #     msg[0],  # Title
-    #     '',  # Body
-    #     'Web Domain Expert <' + settings.EMAIL_HOST_USER + '>',
-    #     [offer.remail, offer.email],
-    #     fail_silently=True,
-    #     html_message=msg[1],
-    #     reply_to=("edomainexpert@gmail.com", ),
-    # )
+
     connection = mail.get_connection()
     connection.open()
     emails = []
