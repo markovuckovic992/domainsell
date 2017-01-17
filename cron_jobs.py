2c5c1861234636fe7b2fa89c309ed3a295c69abc
markovuckovic992 <markovuckovic992@yahoo.com>
2017-01-17 10:28:44 +0100

    Signed-off-by: markovuckovic992 <markovuckovic992@yahoo.com>

---
+++ cron_jobs.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python2.7
+import django
+import sys, traceback
+import os
+from django.core import mail
+os.environ['DJANGO_SETTINGS_MODULE'] = 'DomainScript.settings'
+django.setup()
+
+from domain.models import Offer
+from django.conf import settings
+from maintenance.lib import *
+
+class CronJobs:
+    def __init__(self):
+        pass
+
+    def send(self):
+        offers = Offer.objects.filter(amount__isnull=False)[0:15]
+        connection = mail.get_connection()
+        connection.open()
+        emails = []
+
+        for offer in offers:
+            if offer.phase == 1:
+                to_send = sequnce_1(offer.stage, offer.last_email_date)
+                if to_send:
+                    sub, msg = eval(to_send + '(' + offer.drop + ', ' + offer.name + ')')
+            elif offer.phase == 2:
+                to_send = sequnce_2(offer.stage, offer.last_email_date)
+                if to_send:
+                    sub, msg = eval(to_send + '(' + offer.drop + ', ' + offer.name + ')')
+            elif offer.phase == 3:
+                to_send = sequnce_3(offer.stage, offer.last_email_date)
+                if to_send:
+                    sub, msg = eval(to_send + '(' + offer.drop + ', ' + offer.name + ')')
+
+            if to_send:
+                email = mail.EmailMultiAlternatives(
+                    sub,
+                    '',
+                    'Web Domain Expert <' + settings.EMAIL_HOST_USER + '>',
+                    [offer.remail, offer.email],
+                    reply_to=("support@webdomainexpert.com", ),
+                )
+                email.attach_alternative(msg, "text/html")
+                emails.append(email)
+                stage = offer.stage + 1
+                Offer.objects.filter(id=offer.id).update(stage=stage)
+
+        connection.send_messages(emails)
+        connection.close()
+
+c_j = CronJobs()
+if len(sys.argv) > 1:
+    if sys.argv[1] == 'send':
+        c_j.send()
--- mails/apps.py
+++ mails/apps.py
@@ -6,25 +6,25 @@ from django.apps import AppConfig
 class MailsConfig(AppConfig):
     name = 'mails'
 
-def form_a_msg(domain_name, name):  
-    domain_name = domain_name.upper() 
+def form_a_msg(domain_name, name):
+    domain_name = domain_name.upper()
     subject = 'Thank you for showing interest in ' + domain_name + '  Your free report is ready'
-    msg =  'Hi, ' + name  
+    msg =  'Hi, ' + name
     msg += '<br/>'
     msg += '<br/>'
-    msg += 'Congratulations on making the right choice for your business. Thousands of entrepreneurs have <br/> successfully turned their businesses around using Web Domain Expert is unique domain service. ' 
-    msg += '<br/><br/>' 
-    msg += 'You get a lot more than just a premium domain. ' 
-    msg += '<br/><br/>' 
+    msg += 'Congratulations on making the right choice for your business. Thousands of entrepreneurs have <br/> successfully turned their businesses around using Web Domain Expert is unique domain service. '
+    msg += '<br/><br/>'
+    msg += 'You get a lot more than just a premium domain. '
+    msg += '<br/><br/>'
     msg += 'You will learn How To Double Your Traffic In Less Than A Month For FREE!'
     msg += '<br/><br/>'
-    msg += 'We have not only helped our clients get the best possible domains for their brands, but also <br/> showed them how to <u>drive an enormous amount of traffic</u> to their sites.' 
-    msg += '<br/><br/>' 
-    msg += 'I have received your offer for the domain ' + domain_name + ' and together with my team will do our best to ensure you receive this valuable domain. ' 
-    msg += '<br/><br/>' 
-    msg += 'I will be in touch with you regularly from now on to tell you more about traffic generation and <br/>the best SEO practices today, so you can take them onboard and increase revenue. ' 
-    msg += '<br/>' 
-    msg += 'If you have any question, please feel free to send me an email. I promise to get back to you as<br/> soon as I can. ' 
+    msg += 'We have not only helped our clients get the best possible domains for their brands, but also <br/> showed them how to <u>drive an enormous amount of traffic</u> to their sites.'
+    msg += '<br/><br/>'
+    msg += 'I have received your offer for the domain ' + domain_name + ' and together with my team will do our best to ensure you receive this valuable domain. '
+    msg += '<br/><br/>'
+    msg += 'I will be in touch with you regularly from now on to tell you more about traffic generation and <br/>the best SEO practices today, so you can take them onboard and increase revenue. '
+    msg += '<br/>'
+    msg += 'If you have any question, please feel free to send me an email. I promise to get back to you as<br/> soon as I can. '
     msg += '<br/>'
     msg += '<br/><br/>'
     msg += 'Best regards, <br/>'
