d48989fd424f647c70144864041903545f3bfc41
markovuckovic992 <markovuckovic992@yahoo.com>
2017-02-03 14:38:30 +0100

    Signed-off-by: markovuckovic992 <markovuckovic992@yahoo.com>

--- static/js/index.js
+++ static/js/index.js
@@ -143,8 +143,7 @@ Del = function (id) {
 
 change_amount = function () {
     var html = '<input type="text" id="new_value" placeholder="new offer"/><button onclick="save_new_value">Save</button>';
-    console.log(html)
-    $("#amountfield").html = html;
+    $("#amountfield").html(html);
 }
 
 // save_new_value = function() {
