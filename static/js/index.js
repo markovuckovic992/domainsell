692a9cc261d65d6651d15c02d60e9dbbad8a1a53
markovuckovic992 <markovuckovic992@yahoo.com>
2017-02-03 14:31:17 +0100

    Signed-off-by: markovuckovic992 <markovuckovic992@yahoo.com>

--- static/js/index.js
+++ static/js/index.js
@@ -141,15 +141,15 @@ Del = function (id) {
 }
 
 
-change_amount = function () {
-    var html = '<input type="text" id="new_value" placeholder="new offer"/><button onclick="save_new_value">Save</button>';
-    console.log(html)
-    $("#amountfield").html = html;
-}
+// change_amount = function () {
+//     var html = '<input type="text" id="new_value" placeholder="new offer"/><button onclick="save_new_value">Save</button>';
+//     console.log(html)
+//     $("#amountfield").html = html;
+// }
 
-save_new_value = function() {
-    var new_value = $("#amountfield").val()
-    var html = '<button onclick="change_amount()">' + new_value + '</button>';
-    $("#amountfield").html = html;
-}
+// save_new_value = function() {
+//     var new_value = $("#amountfield").val()
+//     var html = '<button onclick="change_amount()">' + new_value + '</button>';
+//     $("#amountfield").html = html;
+// }
 
