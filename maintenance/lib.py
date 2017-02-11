737600d37a1afde555fb94fde789ef28ab2a7be7
markovuckovic992 <markovuckovic992@yahoo.com>
2017-02-11 14:55:07 +0100

    Signed-off-by: markovuckovic992 <markovuckovic992@yahoo.com>

--- maintenance/lib.py
+++ maintenance/lib.py
@@ -7,7 +7,7 @@ def sequnce_0(stage, last_email_date):
     today = datetime.now()
     case = randint(1, 10)
     if stage == 1:
-        margin = (today - timedelta(days=2)).date()
+        margin = (today - timedelta(days=2))
         if margin > last_email_date:
             return "pr_msg" + str(case)
         else:
@@ -24,28 +24,28 @@ def sequnce_1(stage, last_email_date):
         return "po_msg1"
 
     elif stage == 2:
-        margin = (today - timedelta(days=distance)).date()
+        margin = (today - timedelta(days=distance))
         if margin > last_email_date:
             return "po_msg2"
         else:
             return None
 
     elif stage == 3:
-        margin = (today - timedelta(days=distance)).date()
+        margin = (today - timedelta(days=distance))
         if margin > last_email_date:
             return "po_msg3"
         else:
             return None
 
     elif stage == 4:
-        margin = (today - timedelta(days=distance)).date()
+        margin = (today - timedelta(days=distance))
         if margin > last_email_date:
             return "po_msg4"
         else:
             return None
 
     elif stage == 5:
-        margin = (today - timedelta(days=distance)).date()
+        margin = (today - timedelta(days=distance))
         if margin > last_email_date:
             return "po_msg5"
         else:
@@ -62,77 +62,77 @@ def sequnce_2(stage, last_email_date):
         return "form_a_msg1"
 
     elif stage == 2:
-        margin = (today - timedelta(days=distance)).date()
+        margin = (today - timedelta(days=distance))
         if margin > last_email_date:
             return "form_a_msg2"
         else:
             return None
 
     elif stage == 3:
-        margin = (today - timedelta(days=distance)).date()
+        margin = (today - timedelta(days=distance))
         if margin > last_email_date:
             return "form_a_msg3"
         else:
             return None
 
     elif stage == 4:
-        margin = (today - timedelta(days=distance)).date()
+        margin = (today - timedelta(days=distance))
         if margin > last_email_date:
             return "form_a_msg4"
         else:
             return None
 
     elif stage == 5:
-        margin = (today - timedelta(days=distance)).date()
+        margin = (today - timedelta(days=distance))
         if margin > last_email_date:
             return "form_a_msg5"
         else:
             return None
 
     elif stage == 6:
-        margin = (today - timedelta(days=distance)).date()
+        margin = (today - timedelta(days=distance))
         if margin > last_email_date:
             return "form_a_msg6"
         else:
             return None
 
     elif stage == 7:
-        margin = (today - timedelta(days=distance)).date()
+        margin = (today - timedelta(days=distance))
         if margin > last_email_date:
             return "form_a_msg7"
         else:
             return None
 
     elif stage == 8:
-        margin = (today - timedelta(days=distance)).date()
+        margin = (today - timedelta(days=distance))
         if margin > last_email_date:
             return "form_a_msg8"
         else:
             return None
 
     elif stage == 9:
-        margin = (today - timedelta(days=distance)).date()
+        margin = (today - timedelta(days=distance))
         if margin > last_email_date:
             return "form_a_msg9"
         else:
             return None
 
     elif stage == 10:
-        margin = (today - timedelta(days=distance)).date()
+        margin = (today - timedelta(days=distance))
         if margin > last_email_date:
             return "form_a_msg10"
         else:
             return None
 
     elif stage == 11:
-        margin = (today - timedelta(days=distance)).date()
+        margin = (today - timedelta(days=distance))
         if margin > last_email_date:
             return "form_a_msg11"
         else:
             return None
 
     elif stage == 12:
-        margin = (today - timedelta(days=distance)).date()
+        margin = (today - timedelta(days=distance))
         if margin > last_email_date:
             return "form_a_msg12"
         else:
