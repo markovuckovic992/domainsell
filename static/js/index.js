f6828767ce6e708bdc61b4b86ad0df47a68cfb8e
markovuckovic992 <markovuckovic992@yahoo.com>
2017-03-06 11:20:09 +0100

    Signed-off-by: markovuckovic992 <markovuckovic992@yahoo.com>

--- static/js/index.js
+++ static/js/index.js
@@ -30,7 +30,7 @@ make_offer = function (base_id, hash, ip, name, contact, email) {
             'X-CSRFToken': csrftoken,
         },
         sucess: function(msg) {
-            location.href = 'http://webdomainexpert.com/thank-you/';
+            window.location = 'http://webdomainexpert.com/thank-you/';
         }
 	})
 }
