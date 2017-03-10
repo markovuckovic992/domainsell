d59909a4be9dee4697dfd34b5e6c436f8f3efc09
markovuckovic992 <markovuckovic992@yahoo.com>
2017-03-10 11:06:05 +0100

    Signed-off-by: markovuckovic992 <markovuckovic992@yahoo.com>

--- mails/views.py
+++ mails/views.py
@@ -31,10 +31,7 @@ def sales_page(request):
         return render_to_response('_404.html', {})
     return render_to_response('sales.html', {'drop': entry.drop, 'base_id': entry.base_id, 'hash': hash_base_id, 'amount':entry.amount})
 
-@csrf_exempt
 def process_offer(request):
-    print request.GET.keys(), ' GET KEYS!'
-    print request.POST.keys(), ' POST KEYS!'
     hash_ = request.POST['hash']
     amount = request.POST['amount']
     name = request.POST['name'].lower().capitalize()
@@ -164,3 +161,82 @@ def hello(request):
         return HttpResponse(json.dumps({'greeting': 'hello'}), content_type="application/json")
     response = {'drop': entry.drop, 'base_id': entry.base_id, 'hash': hash_base_id, 'amount':entry.amount}
     return HttpResponse(json.dumps(response), content_type="application/json")
+
+
+@csrf_exempt
+def process_offer_r(request):
+    hash_ = request.GET['hash']
+    amount = request.GET['amount']
+    name = request.GET['name'].lower().capitalize()
+    email = request.GET['email']
+    contact = request.GET['contact']
+
+    match = geolite2.lookup(request.GET['ip'])
+    code = match.country
+    offer_id = random.randint(1000000, 9999999)
+    date = datetime.now().date()
+    Offer.objects.filter(hash_base_id=hash_).update(
+        amount=amount,
+        offer_id=offer_id,
+        date=date,
+        name=name,
+        email=email,
+        contact=contact,
+        code=code,
+        done=0,
+        phase=1,
+        stage=2,
+        last_email_date=timezone.now()
+    )
+
+    offr = Offer.objects.get(hash_base_id=hash_)
+    msg = '''
+        lead= ''' + str(offr.lead) + '\n' + ''',
+        drop= ''' + str(offr.drop) + '\n' + ''',
+        amount= ''' + str(amount) + '\n' + ''',
+        offer_id= ''' + str(offer_id) + '\n' + ''',
+        date=''' + str(date) + '\n' + ''',
+        name=''' + str(name) + '\n' + ''',
+        whois email=''' + str(offr.remail) + '\n' + ''',
+        email=''' + str(email) + '\n' + ''',
+        contact=''' + str(contact) + '\n' + ''',
+        code=''' + str(code) + '\n' + ''',
+    '''
+    send_mail(
+        "Domain offer",  # Title
+        msg,  # Body
+        'Report <' + settings.EMAIL_HOST_USER + '>',
+        ['support@webdomainexpert.com'],
+        fail_silently=False,
+    )
+
+    offer = Offer.objects.get(hash_base_id=hash_)
+    msg = form_a_msg(offer.drop, name)
+
+    if offer.email != offer.remail:
+        sender = [offer.remail, offer.email]
+    else:
+        sender = [offer.remail]
+
+    connection = mail.get_connection(host='smtp.gmail.com',
+                                          port=587,
+                                          username='support@webdomainexpert.com',
+                                          password='asdQWE123',
+                                          use_tls=True)
+    connection.open()
+    emails = []
+
+    email = mail.EmailMultiAlternatives(
+        msg[0],  # Title
+        '',  # Body
+        'Web Domain Expert <support@webdomainexpert.com>',
+        [offer.remail, offer.email],
+        reply_to=("support@webdomainexpert.com", ),
+    )
+    email.attach_alternative(msg[1], "text/html")
+    emails.append(email)
+
+    connection.send_messages(emails)
+    connection.close()
+
+    return HttpResponse('{"status": "success"}', content_type="application/json")
