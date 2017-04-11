5bc5532a96564cafa9671a58d217b274636eb07d
markovuckovic992 <markovuckovic992@yahoo.com>
2017-04-11 16:20:51 +0200

    Signed-off-by: markovuckovic992 <markovuckovic992@yahoo.com>

--- mails/views.py
+++ mails/views.py
@@ -43,75 +43,76 @@ def sales_page_test(request):
     return render_to_response('sales_test.html', {'drop': entry.drop, 'base_id': entry.base_id, 'hash': hash_base_id, 'amount':entry.amount})
 
 def send_emails(hash_, amount, name, email, contact, ip):
-    print '---------1----------'
-    match = geolite2.lookup(ip)
-    code = match.country
-    offer_id = random.randint(1000000, 9999999)
-    date = datetime.now().date()
-    Offer.objects.filter(hash_base_id=hash_).update(
-        amount=amount,
-        offer_id=offer_id,
-        date=date,
-        name=name,
-        email=email,
-        contact=contact,
-        code=code,
-        done=0,
-        phase=1,
-        stage=2,
-        last_email_date=timezone.now()
-    )
+    try:
+        match = geolite2.lookup(ip)
+        code = match.country
+        offer_id = random.randint(1000000, 9999999)
+        date = datetime.now().date()
+        Offer.objects.filter(hash_base_id=hash_).update(
+            amount=amount,
+            offer_id=offer_id,
+            date=date,
+            name=name,
+            email=email,
+            contact=contact,
+            code=code,
+            done=0,
+            phase=1,
+            stage=2,
+            last_email_date=timezone.now()
+        )
 
-    offr = Offer.objects.get(hash_base_id=hash_)
-    msg = '''
-        lead= ''' + str(offr.lead) + '\n' + ''',
-        drop= ''' + str(offr.drop) + '\n' + ''',
-        amount= ''' + str(amount) + '\n' + ''',
-        offer_id= ''' + str(offer_id) + '\n' + ''',
-        date=''' + str(date) + '\n' + ''',
-        name=''' + str(name) + '\n' + ''',
-        whois email=''' + str(offr.remail) + '\n' + ''',
-        email=''' + str(email) + '\n' + ''',
-        contact=''' + str(contact) + '\n' + ''',
-        code=''' + str(code) + '\n' + ''',
-    '''
-    send_mail(
-        "Domain offer",  # Title
-        msg,  # Body
-        'Report <' + settings.EMAIL_HOST_USER + '>',
-        ['support@webdomainexpert.com'],
-        fail_silently=True,
-    )
+        offr = Offer.objects.get(hash_base_id=hash_)
+        msg = '''
+            lead= ''' + str(offr.lead) + '\n' + ''',
+            drop= ''' + str(offr.drop) + '\n' + ''',
+            amount= ''' + str(amount) + '\n' + ''',
+            offer_id= ''' + str(offer_id) + '\n' + ''',
+            date=''' + str(date) + '\n' + ''',
+            name=''' + str(name) + '\n' + ''',
+            whois email=''' + str(offr.remail) + '\n' + ''',
+            email=''' + str(email) + '\n' + ''',
+            contact=''' + str(contact) + '\n' + ''',
+            code=''' + str(code) + '\n' + ''',
+        '''
+        send_mail(
+            "Domain offer",  # Title
+            msg,  # Body
+            'Report <' + settings.EMAIL_HOST_USER + '>',
+            ['support@webdomainexpert.com'],
+            fail_silently=True,
+        )
 
-    offer = Offer.objects.get(hash_base_id=hash_)
-    msg = form_a_msg(offer.drop, name)
+        offer = Offer.objects.get(hash_base_id=hash_)
+        msg = form_a_msg(offer.drop, name)
 
-    if offer.email != offer.remail:
-        sender = [offer.remail, offer.email]
-    else:
-        sender = [offer.remail]
+        if offer.email != offer.remail:
+            sender = [offer.remail, offer.email]
+        else:
+            sender = [offer.remail]
 
-    connection = mail.get_connection(host='smtp.gmail.com',
-                                          port=587,
-                                          username='support@webdomainexpert.com',
-                                          password='asdQWE123',
-                                          use_tls=True)
-    connection.open()
-    emails = []
+        connection = mail.get_connection(host='smtp.gmail.com',
+                                              port=587,
+                                              username='support@webdomainexpert.com',
+                                              password='asdQWE123',
+                                              use_tls=True)
+        connection.open()
+        emails = []
 
-    email = mail.EmailMultiAlternatives(
-        msg[0],  # Title
-        '',  # Body
-        'Web Domain Expert <support@webdomainexpert.com>',
-        [offer.remail, offer.email],
-        reply_to=("support@webdomainexpert.com", ),
-    )
-    email.attach_alternative(msg[1], "text/html")
-    emails.append(email)
+        email = mail.EmailMultiAlternatives(
+            msg[0],  # Title
+            '',  # Body
+            'Web Domain Expert <support@webdomainexpert.com>',
+            [offer.remail, offer.email],
+            reply_to=("support@webdomainexpert.com", ),
+        )
+        email.attach_alternative(msg[1], "text/html")
+        emails.append(email)
 
-    connection.send_messages(emails)
-    connection.close()
-    print '---------2----------'
+        connection.send_messages(emails)
+        connection.close()
+    except:
+        print traceback.format_exc()
 
 def process_offer(request):
     hash_ = request.POST['hash']
